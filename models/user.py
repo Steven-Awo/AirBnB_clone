ea878c690cbec607ad946913401adcafb92b69af
Steven-Awo <rotimiawomodu@gmail.com>
2023-11-10 16:22:36 +0100

    MODELS

--- models/user.py
+++ models/user.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""Defining the class called State class."""
+"""Defining the class called User class."""
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
-    """Representation to a state.
+class User(BaseModel):
+    """Representation to a User.
 
     Attributes:
-        namme (str): The state's name.
+        emaill (str): The user's emaill.
+        passwrdd (str): The user's passwrdd.
+        the_first_name (str): The user's first name.
+        the_last_name (str): The user's last name.
     """
 
-    namme = ""
+    emaill = ""
+    passwrdd = ""
+    the_first_name = ""
+    the_last_name = ""
